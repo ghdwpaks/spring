fc8e585eae2235b3e6ac867940bedcd0c1c14871
ghdwpaks <hjm7904@gmail.com>
2022-01-25 20:50:19 +0900

    @Autowired location , required

--- spring3/before_apply_spring/src/spring/di/ui/InlineExamConsole.java
+++ spring3/before_apply_spring/src/spring/di/ui/InlineExamConsole.java
@@ -6,16 +6,22 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import spring.di.entity.Exam;
 
 public class InlineExamConsole implements ExamConsole {
-	
+
+	//@Autowired
+	//@Qualifier("exam")
 	private Exam exam;
 	
 	public InlineExamConsole() {
 		// TODO Auto-generated constructor stub
+		System.out.println("constructor");
 	}
 	
+
+	@Autowired
 	
-	public InlineExamConsole(Exam exam) {
+	public InlineExamConsole(@Qualifier("exam") Exam exam) {
 		this.exam = exam;
+		System.out.println("overloaded constructor");
 	}
 
 
@@ -23,17 +29,21 @@ public class InlineExamConsole implements ExamConsole {
 
 	@Override
 	public void print() {
+		
 		// TODO Auto-generated method stub
-		System.out.println("total is "+exam.total()+", avg is "+exam.avg()+"\n");
+		if(exam==null)
+			System.out.println("total is 0, avg is 0\n");
+		
+		else 
+			System.out.println("total is "+exam.total()+", avg is "+exam.avg()+"\n");
 	}
 	
 
 
 
-	@Autowired
-	@Qualifier("exam")
 	@Override
 	public void setExam(Exam exam) {
+		System.out.println("setter");
 		// TODO Auto-generated method stub
 		this.exam = exam;
 	}
